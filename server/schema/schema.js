7e555d7eb078a14f40f7d76505157272a4e51270
Hasibul-99 <hasibul_uap@outlook.com>
2019-03-15 23:52:02 +0600

    resolve function to get the result

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+var books = [
+    {name: "abc", genre: "Fantasy", id: '1'},
+    {name: "def", genre: "History", id: '2'},
+    {name: "ghi", genre: "Fire", id: '3'}
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -10,3 +17,23 @@ const BookType = new GraphQLObjectType({
         genre: {type: GraphQLString}
     })
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args) {
+                // code to get data from db / other source
+
+               return _.find(books, {id: args.id});
+            }
+        }
+    }
+})
+
+// Creating a new graphql shema and inside this pass through RootQuery
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})

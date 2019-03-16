212770a8f6fafcb06db887b7ef5067dd1d5fb6f4
Hasibul-99 <hasibul_uap@outlook.com>
2019-03-16 18:29:12 +0600

    make font end to get the data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,8 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema,
+GraphQLID} = graphql;
 
 var books = [
     {name: "abc", genre: "Fantasy", id: '1'},
@@ -12,7 +13,7 @@ var books = [
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
@@ -23,7 +24,7 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent, args) {
                 // code to get data from db / other source
 
